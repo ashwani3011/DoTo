b0cd24c2b9748a5ef70bb4f1cc29f5339f0a7dd2
= <=>
2022-01-31 15:48:02 +0530

    uploading profile pic - ref, put, getDownloadURL

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCbCaL33G2SsK2vgZMLVyxLH0EQZSSHvXg",
@@ -19,8 +20,9 @@ firebase.initializeApp(firebaseConfig);
 
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
+const projectStorage = firebase.storage();
 
 // timestamp
 
 const timestamp = firebase.firestore.Timestamp;
-export { projectFirestore, projectAuth, timestamp };
+export { projectFirestore, projectAuth, timestamp, projectStorage };
--- src/pages/hooks/useSignup.js
+++ src/pages/hooks/useSignup.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { projectAuth } from "../firebase/config";
+import { projectAuth, projectStorage } from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
 
 export const useSignup = () => {
@@ -8,7 +8,7 @@ export const useSignup = () => {
   const [isPending, setIsPending] = useState(false);
   const { dispatch } = useAuthContext();
 
-  const signup = async (email, password, displayName) => {
+  const signup = async (email, password, displayName, thumbnail) => {
     setError(null);
     setIsPending(true);
 
@@ -19,12 +19,21 @@ export const useSignup = () => {
         password
       );
 
+      // to handle the case when we do not get any response back
       if (!res) {
         throw new Error("Could not complete signup");
       }
 
-      // add display name to user
-      await res.user.updateProfile({ displayName });
+      // upload user thumbnail
+      // we are creating folder for each thumbnail and the name is uid
+      const uploadPath = `thumbnails/${res.user.uid}/${thumbnail.name}`;
+      //ref method except the path where we want to store an item
+      const img = await projectStorage.ref(uploadPath).put(thumbnail);
+      // ref - get this reference
+      const imgUrl = await img.ref.getDownloadURL();
+
+      // add display AND PHOTO_URL name to user
+      await res.user.updateProfile({ displayName, photoURL: imgUrl });
 
       // dispatch login action
       dispatch({ type: "LOGIN", payload: res.user });
