caaf4ba64ce525cf05f8f4274818ef33240d991b
= <=>
2022-02-01 12:48:30 +0530

    add and list comment

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import { timestamp } from "../../firebase/config";
 import { useAuthContext } from "../../hooks/useAuthContext";
+import { useFirestore } from "../../hooks/useFirestore";
+import Avatar from "../../components/Avatar";
 
-export default function ProjectComments() {
+export default function ProjectComments({ project }) {
   const { user } = useAuthContext();
+  const { updateDocument, response } = useFirestore("projects");
   const [newComment, setNewComment] = useState("");
 
   const handleSubmit = async (e) => {
@@ -16,18 +19,41 @@ export default function ProjectComments() {
       createdAt: timestamp.fromDate(new Date()),
       id: Math.random(),
     };
-    console.log(commentToAdd);
+
+    await updateDocument(project.id, {
+      comments: [...project.comments, commentToAdd],
+    });
+    if (!response.error) {
+      setNewComment("");
+    }
   };
 
   return (
     <div className="project-comments">
       <h4>Project Comments</h4>
 
+      <ul>
+        {project.comments.length > 0 &&
+          project.comments.map((comment) => (
+            <li key={comment.id}>
+              <div className="comment-author">
+                <Avatar src={comment.photoURL} />
+                <p>{comment.displayName}</p>
+              </div>
+              <div className="comment-date">
+                <p>date here</p>
+              </div>
+              <div className="comment-content">
+                <p>{comment.content}</p>
+              </div>
+            </li>
+          ))}
+      </ul>
+
       <form className="add-comment" onSubmit={handleSubmit}>
         <label>
           <span>Add new comment:</span>
           <textarea
-            required
             onChange={(e) => setNewComment(e.target.value)}
             value={newComment}
           ></textarea>
