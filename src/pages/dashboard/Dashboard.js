4b970f037dbfb0a0e11a052538f7e3561b904cbb
= <=>
2022-02-01 11:34:41 +0530

    making project grid in dashboard

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,5 +1,19 @@
+import { useCollection } from "../../hooks/useCollection";
+
+// components
+import ProjectList from "../../components/ProjectList";
+
+// styles
 import "./Dashboard.css";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const { documents, error } = useCollection("projects");
+
+  return (
+    <div>
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error}</p>}
+      {documents && <ProjectList projects={documents} />}
+    </div>
+  );
 }
